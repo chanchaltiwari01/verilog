1b42b09a469afdbcd20aa80d9042cc7cf8f1215a
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-12 20:12:16 +0530

    Update net.v

--- data_type/net.v
+++ data_type/net.v
@@ -1,13 +1,12 @@
 module nets_data_type ;
   wire a, b ;
+  
+  assign  #1 {a,b} = 2'b01 ; 
+  assign  #2 {a,b} = 2'b10 ; 
+  assign  #3 {a,b} = 2'b01 ; 
 
   initial begin
-    $display("a=%0b, b=%0b " ,a,b);
-    #1 ;
-    a=1'b0; b=1'b1;
-    $display("a=%0b, b=%0b " ,a,b);
-    #1;
-    a=1'b1; b=1'b0;
-    $display("a=%0b, b=%0b " ,a,b);
+    $display("a=%0b, b=%0b",a,b);
   end 
+  
 endmodule 
