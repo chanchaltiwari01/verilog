04cb9a26dc243d02e1aff677c4ebcf3e011e71e2
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-13 00:30:28 +0530

    Create write.v

---
+++ System Task/write.v
@@ -0,0 +1,10 @@
+//$write work same as $display 
+//except it dose not automatically add new line
+
+module write  ;
+initial begin
+$write("jay shree ram");
+$write ("shiyavar ramchandra ki jay ");
+$write("satya sanan dharam ki jai ho");
+end 
+endmodule
