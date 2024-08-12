b06ed093a20a1b32a0487e15b6c33e34fa9824f1
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-12 21:14:50 +0530

    Create concatenation.v

---
+++ operators/concatenation.v
@@ -0,0 +1,11 @@
+module con();
+reg[1:0] a = 2'b01;
+reg[2:0] b = 3'b110;
+reg [1:0]c = 2'b10;
+
+initial begin 
+  $display("a_b_c=%b",{a,b,c});
+  $display("a[0]_b[0]_c[0]=%b",{a[0],b[0],c[0]});
+  $display("a, 4'b1111=%b",{a,4'b1111});
+end
+endmodule
