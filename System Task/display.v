9379563c8755e5133935c85d290c4baef2950bce
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-13 00:29:57 +0530

    Create display.v

---
+++ System Task/display.v
@@ -0,0 +1,10 @@
+//$display use to display of value and statement
+//it is execute at active region 
+//execute in 0 simulaton time/ insert new line by default 
+module display ;
+initial begin
+$display("jay shree ram");
+$display ("shiyavar ramchandra ki jay ");
+$display("satya sanan dharam ki jai ho");
+end 
+endmodule
