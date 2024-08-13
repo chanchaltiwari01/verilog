2dd254d66ccf20b45c439fcffdd4e8910e817991
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-13 18:33:52 +0530

    Create or_gate.v

---
+++ Digital Design with Verilog/combinational circuit/gates/or_gate.v
@@ -0,0 +1,22 @@
+module or_gate (input a ,b,
+                output c );
+  assign c  = a | b ;
+  //or o1(c,a,b);
+endmodule 
+
+module tb; 
+  reg a ,b ;
+  wire c ;
+
+  or_gate dut(.a(a),
+              .b(b),
+              .c(c) );
+  initial begin
+    $monitor("[%0t] : a=%0d , b=%0d, c=%0d",$time,a,b,c);
+    for(int i=0 ; i<4 ; i=i+1) begin
+      {a,b} = i ;
+      #10 ;
+    end 
+    $stop ;
+  end 
+endmodule 
